9f57e1540dff10f034cb90932a533a9ea71bc00b
Xianhua Liu <robertliu325@yahoo.com>
2019-12-29 16:07:37 -0500

    removed duplicate db files

--- json4orm-web/src/test/resources/scripts/Class.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE class(
-   class_id serial PRIMARY KEY,
-   name VARCHAR (255) NOT NULL,
-   school_id integer 
-);
--- json4orm-web/src/test/resources/scripts/School.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE school(
-   school_id serial PRIMARY KEY,
-   name VARCHAR (255) NOT NULL
-);
